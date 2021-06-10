0f8b6c89fb25f0fd1a3e014afa540dc70471cc3f
callmeliuchu <19931108github>
2021-06-10 21:56:24 +0800

    fix

---
+++ chapter3/build/CMakeFiles/Rasterizer.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/liuchu/Downloads/Assignment1/CG-learning/chapter3/Texture.cpp" "/Users/liuchu/Downloads/Assignment1/CG-learning/chapter3/build/CMakeFiles/Rasterizer.dir/Texture.cpp.o"
+  "/Users/liuchu/Downloads/Assignment1/CG-learning/chapter3/Triangle.cpp" "/Users/liuchu/Downloads/Assignment1/CG-learning/chapter3/build/CMakeFiles/Rasterizer.dir/Triangle.cpp.o"
+  "/Users/liuchu/Downloads/Assignment1/CG-learning/chapter3/main.cpp" "/Users/liuchu/Downloads/Assignment1/CG-learning/chapter3/build/CMakeFiles/Rasterizer.dir/main.cpp.o"
+  "/Users/liuchu/Downloads/Assignment1/CG-learning/chapter3/rasterizer.cpp" "/Users/liuchu/Downloads/Assignment1/CG-learning/chapter3/build/CMakeFiles/Rasterizer.dir/rasterizer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  "/usr/local/Cellar/opencv/4.1.2/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
