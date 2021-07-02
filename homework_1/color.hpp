2a5d29ca06bc0fd95e20fd93858112bcccd3eba4
liuchu <chuliu@linkdoc.com>
2021-07-02 21:01:22 +0800

    fix

---
+++ homework_1/color.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "Vector.hpp"
+#include "rtweekend.hpp"
+#include <iostream>
+
+
+void write_color(std::ostream &out, Vector3f pixel_color,int sample_per_pix) {
+    // Write the translated [0,255] value of each color component.
+
+    auto r = pixel_color.x;
+    auto g = pixel_color.y;
+    auto b = pixel_color.z;
+    auto scale = 1.0 / sample_per_pix;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+    r = sqrt(r);
+    g = sqrt(g);
+    b = sqrt(b);
+
+
+    out << static_cast<int>(256 * clamp(r,0,0.9999)) << ' '
+        << static_cast<int>(256 * clamp(g,0,0.9999)) << ' '
+        << static_cast<int>(256 * clamp(b,0,0.9999)) << '\n';
+}
---
+++ homework_1/hittable.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "ray.hpp"
+
+struct hit_record {
+    Vector3f p;
+    Vector3f normal;
+    double t;
+    bool front_face;
+    inline void set_face_normal(const ray& r,const Vector3f& outward_normal){
+        front_face = dotProduct(r.direction(),outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
+};
+
+class hittable{
+    public:
+       virtual bool hit(const ray& r,double t_min,double t_max,hit_record& rec)const = 0;
+};
