6f57f32d7bf2e9f0adf83a26cdfc633e2f2386bc
liuchu <chuliu@linkdoc.com>
2021-07-03 09:24:49 +0800

    fix

--- homework/main.cpp
+++ homework/main.cpp
@@ -37,7 +37,7 @@ int main() {
 
     hittable_list world;
     world.add(make_shared<Sphere>(Vector3f(0,0,-1),0.5));
-    world.add(make_shared<Sphere>(Vector3f(0,-100.5,-1), 100));
+    // world.add(make_shared<Sphere>(Vector3f(0,-100.5,-1), 100));
 
     //camera
     camera cam;
