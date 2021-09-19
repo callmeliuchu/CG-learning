285f72f2f5cee19289e0ad116b24bdee413d3065
liuchu <chuliu@linkdoc.com>
2021-09-19 19:37:55 +0800

    fix

---
+++ homework201/torch_example.py
@@ -0,0 +1,28 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor,Lambda,Compose
+import matplotlib.pyplot as plt
+
+training_data = datasets.FashionMNIST(
+    root='data',
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+test_data = datasets.FashionMNIST(
+    root='data',
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
+batch_size = 64
+
+train_dataloader = DataLoader(training_data,batch_size=batch_size)
+test_dataloader = DataLoader(test_data,batch_size=batch_size)
+
+for X in test_dataloader:
+    print(X)
